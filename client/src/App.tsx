dd57796039b4fb36ca18acbf178aacf1868f731e
Morgan Githinji <githinjimorgan@gmail.com>
2022-09-14 10:07:56 -0700

    First commit

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Footer, Loader, Navbar, Services, Transactions, Welcome } from './components';
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen">
+      
+      <div className="gradient-bg-welcome">
+        <div><Navbar /></div>
+        <div><Welcome /></div>
+      </div>
+      
+      <div><Services /></div>
+      <div><Transactions /></div>
+      <div><Footer /></div>
+      <div><Loader /></div>
+      
+    </div>
+  );
+
+}
+
+export default App
