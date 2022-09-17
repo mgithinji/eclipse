33cce222db60e258338fbaecada927207a968729
Morgan Githinji <githinjimorgan@gmail.com>
2022-09-16 22:51:15 -0700

    Smart contract development started

---
+++ smart-contract/scripts/deploy.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+
+  const Transactions = await ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log("Transactions deployed to: ", transactions.address);
+
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = ethers.utils.parseEther("1");
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
